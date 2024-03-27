1f5e8572a52bdfb6918dbb6c1f269850e2372cd9
Aditya Sharma <67048929+extinctsion@users.noreply.github.com>
2024-03-27 18:46:56 +0000

    timeout increased

--- nextjs-dashboard/next.config.js
+++ nextjs-dashboard/next.config.js
@@ -3,5 +3,5 @@ const nextConfig = {};
 module.exports = {
     // time in seconds of no pages generating during static
     // generation before timing out
-    staticPageGenerationTimeout: 1000,
+    staticPageGenerationTimeout: 2000,
   }
